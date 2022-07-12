28947630f9b393daa822f4725858b0964868867e
Daniel Peña Perez <danipenaperez@gmail.com>
2022-07-12 13:58:40 +0200

    changed script

--- build.sh
+++ build.sh
@@ -19,12 +19,18 @@ fi
 mkdir $SITE_OUTPUT_DIR
 
 # Static resources
+echo "Copying Html source to output Bin directory...."
 cp -R ./html $SITE_OUTPUT_DIR
+echo "Copying images source to output Bin directory...."
 cp -R ./images $SITE_OUTPUT_DIR
+echo "Copying Stylesheets source to output Bin directory...."
 cp -R ./stylesheets $SITE_OUTPUT_DIR
+echo "Copying Thirdparty source to output Bin directory...."
 cp -R ./thirdparty $SITE_OUTPUT_DIR
 
+
 # Building Javascript concat and minify
+echo "Concat all Js files...."
 SRC_DIR=$PWD/src
 JS_CONCAT_OUTPUT_DIR=$SRC_DIR/js
 # Remove old folder
@@ -37,8 +43,10 @@ fi
 cd $SRC_DIR
 npm install
 grunt
-
+echo "Concat all Js files....END"
+echo "Moving concat JS to Bin dir...."
 mv $JS_CONCAT_OUTPUT_DIR  $SITE_OUTPUT_DIR
+echo "Moving concat JS to Bin dir....END"
 #Build Image
 #docker build -t fwkcna/mkdocs .
 
